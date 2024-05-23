577d8f86cdd774c1c424a7b9d8730e4a49f4aac5
JoaquinE4 <casarotajoachin@gmail.com>
2024-05-23 02:22:41 -0300

    cards refactor

--- src/app/components/Cards.jsx
+++ src/app/components/Cards.jsx
@@ -1,132 +1,309 @@
-export default function Cards() {
+"use client";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
+
+export default function App() {
+  const [swiperRef, setSwiperRef] = useState(null);
+
+ 
+
   return (
-    <div className="sm:h-[26rem]  bg-[#e1dbf5b0]  justify-center content-center">
-      {/*       <h2 className5" text-center p-4 text-4xl font-semibold ">Productos</h2>
-       */}{" "}
-      <section
-        id="Projects"
-        className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5"
-      >
-        <div className="w-60 bg-[#ffffffda] shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <img
-              src="https://i.ibb.co/zNRf6Vw/IMG-20240124-WA0046-removebg-preview.png"
-              alt="Product"
-              className="h-min w-72 p-5 object-cover rounded-t-xl"
-            />
-            <div className="px-4 py-3 w-72">
-              <p className="text-lg font-bold text-black truncate block capitalize">
-                Mate Ovo
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-black cursor-auto my-3">
-                  $4899
-                </p>
-                <del>
-                  <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
-                </del>
-                <div className="m-3">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    className="bi bi-bag-plus"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                    />
-                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-
-        <div className="w-60 bg-[#ffffffda] shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <img
-              src="https://i.ibb.co/SQ5DXc3/maceta-gato-4-patas-2.png"
-              alt="Product"
-              className="h-min w-72 p-5 object-cover rounded-t-xl"
-            />
-            <div className="px-4 py-3 w-72">
-              <p className="text-lg font-bold text-black truncate block capitalize">
-                Mate Ovo
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-black cursor-auto my-3">
-                  $4899
-                </p>
-                <del>
-                  <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
-                </del>
-                <div className="m-3">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    className="bi bi-bag-plus"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                    />
-                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-        <div className="w-60 bg-[#ffffffda] shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
-          <a href="#">
-            <img
-              src="https://i.ibb.co/zNRf6Vw/IMG-20240124-WA0046-removebg-preview.png"
-              alt="Product"
-              className="h-min w-72 p-5 object-cover rounded-t-xl"
-              
-            />
-            <div className="px-4 py-3 w-72">
-              <p className="text-lg font-bold text-black truncate block capitalize">
-                Mate Ovo
-              </p>
-              <div className="flex items-center">
-                <p className="text-lg font-semibold text-black cursor-auto my-3">
-                  $4899
-                </p>
-                <del>
-                  <p className="text-sm text-gray-600 cursor-auto ml-2">$199</p>
-                </del>
-                <div className="m-3">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="20"
-                    height="20"
-                    fill="currentColor"
-                    className="bi bi-bag-plus"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
-                    />
-                    <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
-                  </svg>
-                </div>
-              </div>
-            </div>
-          </a>
-        </div>
-
-
+    <div className="   p-11 ">
+      
+      <Swiper
+        onSwiper={setSwiperRef}
+        slidesPerView={1}
         
-      </section>
+        centeredSlides={true}
+        spaceBetween={10}
+        pagination={{
+          type: "fraction",
+        }}
+        autoplay={{
+          delay: 1750,
+          disableOnInteraction: false,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+          },
+          1000:{
+            slidesPerView:3
+          }
+        }}
+        navigation={true}
+        modules={[Navigation, Autoplay]}
+        className="  "
+        >
+          
+           <SwiperSlide className="center"   >
+          
+          <div className="w-60    bg-[#ffffff00]    rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+            <a href="#">
+              <img
+                src="https://i.ibb.co/zNRf6Vw/IMG-20240124-WA0046-removebg-preview.png"
+                alt="Product"
+                className="h-min w-72  object-cover rounded-t-xl"
+              />
+              <div className="px-4 py-3 w-72">
+                <p className="text-lg font-bold text-black truncate block capitalize">
+                  Mate Ovo
+                </p>
+                <div className="flex items-center">
+                  <p className="text-lg font-semibold text-black cursor-auto my-3">
+                    $4899
+                  </p>
+                  <del>
+                    <p className="text-sm text-gray-600 cursor-auto ml-2">
+                      $199
+                    </p>
+                  </del>
+                  <div className="m-3">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-bag-plus"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
+                      />
+                      <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </SwiperSlide>
+           <SwiperSlide className="center">
+            
+          <div className="w-60 bg-[#ffffffda] shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+            <a href="#">
+              <img
+                src="https://i.ibb.co/zNRf6Vw/IMG-20240124-WA0046-removebg-preview.png"
+                alt="Product"
+                className="h-min w-72 p-5 object-cover rounded-t-xl"
+              />
+              <div className="px-4 py-3 w-72">
+                <p className="text-lg font-bold text-black truncate block capitalize">
+                  Mate Ovo
+                </p>
+                <div className="flex items-center">
+                  <p className="text-lg font-semibold text-black cursor-auto my-3">
+                    $4899
+                  </p>
+                  <del>
+                    <p className="text-sm text-gray-600 cursor-auto ml-2">
+                      $199
+                    </p>
+                  </del>
+                  <div className="m-3">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-bag-plus"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
+                      />
+                      <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </SwiperSlide>
+           <SwiperSlide className="center">
+            
+          <div className="w-60 bg-[#ffffffda] shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+            <a href="#">
+              <img
+                src="https://i.ibb.co/zNRf6Vw/IMG-20240124-WA0046-removebg-preview.png"
+                alt="Product"
+                className="h-min w-72 p-5 object-cover rounded-t-xl"
+              />
+              <div className="px-4 py-3 w-72">
+                <p className="text-lg font-bold text-black truncate block capitalize">
+                  Mate Ovo
+                </p>
+                <div className="flex items-center">
+                  <p className="text-lg font-semibold text-black cursor-auto my-3">
+                    $4899
+                  </p>
+                  <del>
+                    <p className="text-sm text-gray-600 cursor-auto ml-2">
+                      $199
+                    </p>
+                  </del>
+                  <div className="m-3">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-bag-plus"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
+                      />
+                      <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        </SwiperSlide>
+           <SwiperSlide className="center">
+            
+          <div className="w-60 bg-[#ffffffda] shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+            <a href="#">
+              <img
+                src="https://i.ibb.co/zNRf6Vw/IMG-20240124-WA0046-removebg-preview.png"
+                alt="Product"
+                className="h-min w-72 p-5 object-cover rounded-t-xl"
+              />
+              <div className="px-4 py-3 w-72">
+                <p className="text-lg font-bold text-black truncate block capitalize">
+                  Mate Ovo
+                </p>
+                <div className="flex items-center">
+                  <p className="text-lg font-semibold text-black cursor-auto my-3">
+                    $4899
+                  </p>
+                  <del>
+                    <p className="text-sm text-gray-600 cursor-auto ml-2">
+                      $199
+                    </p>
+                  </del>
+                  <div className="m-3">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-bag-plus"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
+                      />
+                      <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>  
+        </SwiperSlide>
+           <SwiperSlide className="center">
+            
+          <div className="w-60 bg-[#ffffffda] shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+            <a href="#">
+              <img
+                src="https://i.ibb.co/zNRf6Vw/IMG-20240124-WA0046-removebg-preview.png"
+                alt="Product"
+                className="h-min w-72 p-5 object-cover rounded-t-xl"
+              />
+              <div className="px-4 py-3 w-72">
+                <p className="text-lg font-bold text-black truncate block capitalize">
+                  Mate Ovo
+                </p>
+                <div className="flex items-center">
+                  <p className="text-lg font-semibold text-black cursor-auto my-3">
+                    $4899
+                  </p>
+                  <del>
+                    <p className="text-sm text-gray-600 cursor-auto ml-2">
+                      $199
+                    </p>
+                  </del>
+                  <div className="m-3">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-bag-plus"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
+                      />
+                      <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>  
+        </SwiperSlide>
+           <SwiperSlide className="center">
+            
+          <div className="w-60 bg-[#ffffffda] shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+            <a href="#">
+              <img
+                src="https://i.ibb.co/zNRf6Vw/IMG-20240124-WA0046-removebg-preview.png"
+                alt="Product"
+                className="h-min w-72 p-5 object-cover rounded-t-xl"
+              />
+              <div className="px-4 py-3 w-72">
+                <p className="text-lg font-bold text-black truncate block capitalize">
+                  Mate Ovo
+                </p>
+                <div className="flex items-center">
+                  <p className="text-lg font-semibold text-black cursor-auto my-3">
+                    $4899
+                  </p>
+                  <del>
+                    <p className="text-sm text-gray-600 cursor-auto ml-2">
+                      $199
+                    </p>
+                  </del>
+                  <div className="m-3">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      className="bi bi-bag-plus"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M8 7.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V12a.5.5 0 0 1-1 0v-1.5H6a.5.5 0 0 1 0-1h1.5V8a.5.5 0 0 1 .5-.5z"
+                      />
+                      <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>  
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
